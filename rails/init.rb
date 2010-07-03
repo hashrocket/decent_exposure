1861ec4ab3131ef643d0a5699c95260628e9b19d
Stephen Caudill <scaudill@gmail.com>
2010-07-02 23:23:36 -0400

    Improve Rails' default_exposure

--- rails/init.rb
+++ rails/init.rb
@@ -7,8 +7,33 @@ end
 ActionController::Base.class_eval do
   extend DecentExposure
   superclass_delegating_accessor :_default_exposure
+
   default_exposure do |name|
-    model_class = name.to_s.classify.constantize
-    model_class.find(params["#{name}_id"] || params['id'])
+    self._designation = name.to_s.pluralize
+    if id = params["#{name}_id"] || params[:id]
+      _collection.find(id).tap do |r|
+        r.attributes = params[name] unless request.get?
+      end
+    else
+      _collection.new(params[name])
+    end
   end
+
+  private
+  attr_accessor :_designation
+  attr_writer :_collection_name
+
+  def _collection_name
+    @_collection_name || _designation
+  end
+
+  def _collection
+    unless self.class.method_defined?(_collection_name)
+      self.class.expose(_collection_name) do
+        _collection_name.classify.constantize.scoped({})
+      end
+    end
+    send(_collection_name)
+  end
+
 end
--- spec/lib/default_exposure_spec.rb
+++ spec/lib/default_exposure_spec.rb
@@ -3,56 +3,91 @@ require 'action_controller'
 
 require File.join(File.dirname(__FILE__), '..', '..', 'rails', 'init.rb')
 
-class Resource; end
-
 ActionController::Base.class_eval do
-  def params; {'resource_id' => 42}; end
+  def params; HashWithIndifferentAccess.new(:resource_id => 42); end
   expose :resource
+end
+
+class Resource
+  def self.scoped(opts); Resource; end
+  def self.find(*args); end
+  def initialize(*args); end
 end
 
 describe "Rails' integration:", DecentExposure do
   let(:controller) { ActionController::Base.new }
+  let(:request) { mock(:get? => true) }
+  before { controller.stubs(:request).returns(request) }
 
   it 'extends ActionController::Base' do
     ActionController::Base.should respond_to(:expose)
   end
 
   context '.default_exposure' do
-    it "calls find with params[:resource_id] on the resource's class" do
-      Resource.expects(:find).with(42)
-      controller.resource
+    context 'when no collection method exists' do
+      it 'creates a collection method to scope from' do
+        controller.resource
+        controller.methods.should include('resources')
+      end
+    end
+    context 'when a collection method exists' do
+      before do
+        def controller.resources; end
+      end
+      it 'uses the existing collection method' do
+        controller.expects(:resources).returns(Resource)
+        controller.resource
+      end
     end
 
-    context 'when there is no :resource_id in params' do
-      before do
-        ActionController::Base.class_eval do
-          def params; {'id' => 73}; end
-        end
+    context 'when either :resource_id or :id are present in params' do
+      it "calls find with params[:resource_id] on the resource's class" do
+        Resource.expects(:find).with(42)
+        controller.resource
       end
 
-      it "calls find with params[:id] on the resource's class" do
-        Resource.expects(:find).with(73)
+      context 'when there is no :resource_id in params' do
+        before do
+          def controller.params; {:id => 73}; end
+        end
+
+        it "calls find with params[:id] on the resource's class" do
+          Resource.expects(:find).with(73)
+          controller.resource
+        end
+      end
+    end
+    context 'when there are no ids in params' do
+      before do
+        def controller.params; {:resource => {:name => 'bob'}} end
+      end
+      it 'calls new with params[:resouce_name]' do
+        Resource.expects(:new).with({:name => 'bob'})
         controller.resource
       end
     end
   end
 
   context 'within descendant controllers' do
-    class ResourceController < ActionController::Base; end
-
-    let(:my_controller) { ResourceController.new }
+    let(:resource_controller) { Class.new(ActionController::Base) }
+    let(:instance) { resource_controller.new }
+    before do
+      instance.stubs(:request).returns(request)
+      def instance.params; HashWithIndifferentAccess.new(:resource_id => 42); end
+      resource_controller.expose :resource
+    end
 
     it 'inherits the default_exposure' do
       Resource.stubs(:find).returns('resource')
-      my_controller.resource.should == 'resource'
+      instance.resource.should == 'resource'
     end
 
     it 'allows you to override the default_exposure' do
-      ResourceController.class_eval do
+      resource_controller.class_eval do
         default_exposure {|name| name.to_s}
         expose :overridden
       end
-      my_controller.overridden.should == 'overridden'
+      instance.overridden.should == 'overridden'
     end
 
     it 'does not override the default in ancestors' do
