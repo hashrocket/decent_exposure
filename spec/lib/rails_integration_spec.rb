db3fb4b57d91e5ad23b289142dbd756f9d6ce004
Stephen Caudill <scaudill@gmail.com>
2010-07-16 15:06:25 -0400

    Yet more spec refactoring

--- spec/lib/rails_integration_spec.rb
+++ spec/lib/rails_integration_spec.rb
@@ -10,15 +10,13 @@ class Resource
 end
 
 describe "Rails' integration:", DecentExposure do
-  let(:action_controller) { ActionController::Base }
-  let(:instance) { action_controller.new }
+  let(:controller) { Class.new(ActionController::Base) }
+  let(:instance) { controller.new }
   let(:request) { mock(:get? => true) }
+  let(:params) { HashWithIndifferentAccess.new(:resource_id => 42) }
 
   before do
-    action_controller.class_eval do
-      def params; HashWithIndifferentAccess.new(:resource_id => 42); end
-      expose(:resource)
-    end
+    controller.expose(:resource)
     instance.stubs(:request).returns(request)
   end
 
@@ -28,76 +26,13 @@ describe "Rails' integration:", DecentExposure do
     end
   end
 
-  context '.default_exposure' do
-    it 'is available to ActionController::Base' do
-      ActionController::Base.should respond_to(:default_exposure)
-    end
-
-    context 'when no collection method exists' do
-      it 'creates a collection method to scope from' do
-        instance.resource
-        instance.methods.should include('resources')
-      end
-    end
-
-    context 'when a collection method exists' do
-      let(:controller){ Class.new(ActionController::Base) }
-      let(:instance){ controller.new }
-      before { class Person < Resource; end }
-      context 'and the collection can be scoped' do
-        let(:collection){ mock(:scoped => [self]) }
-        before{ controller.expose(:person) }
-        it 'uses the existing collection method' do
-          instance.stubs(:people).returns(collection)
-          collection.expects(:new)
-          instance.person
-        end
-      end
-      context 'when the collection can not be scoped' do
-        let(:collection){ mock }
-        before{ controller.expose(:person) }
-        it 'falls back to the singularized constant' do
-          instance.stubs(:people).returns(collection)
-          Person.expects(:new)
-          instance.person
-        end
-      end
-    end
-
-    context 'when either :resource_id or :id are present in params' do
-      it "calls find with params[:resource_id] on the resource's class" do
-        Resource.expects(:find).with(42)
-        instance.resource
-      end
-
-      context 'when there is no :resource_id in params' do
-        before do
-          def instance.params; {:id => 73}; end
-        end
-
-        it "calls find with params[:id] on the resource's class" do
-          Resource.expects(:find).with(73)
-          instance.resource
-        end
-      end
-    end
-    context 'when there are no ids in params' do
-      before do
-        def instance.params; {:resource => {:name => 'bob'}} end
-      end
-      it 'calls new with params[:resouce_name]' do
-        Resource.expects(:new).with({:name => 'bob'})
-        instance.resource
-      end
-    end
-  end
-
   context 'within descendant controllers' do
     let(:resource_controller) { Class.new(ActionController::Base) }
     let(:instance) { resource_controller.new }
+
     before do
       instance.stubs(:request).returns(request)
-      def instance.params; HashWithIndifferentAccess.new(:resource_id => 42); end
+      instance.stubs(:params).returns(params)
       resource_controller.expose :resource
     end
 
@@ -117,6 +52,79 @@ describe "Rails' integration:", DecentExposure do
     it 'does not override the default in ancestors' do
       Resource.stubs(:find).returns('preserved')
       instance.resource.should == 'preserved'
+    end
+  end
+
+  context '.default_exposure' do
+    before do
+      instance.stubs(:params).returns(params)
+    end
+
+    it 'is available to ActionController::Base' do
+      ActionController::Base.should respond_to(:default_exposure)
+    end
+
+    context 'when no collection method exists' do
+      it 'creates a collection method to scope from' do
+        instance.resource
+        instance.methods.should include('resources')
+      end
+    end
+
+    context 'when a collection method exists' do
+      let(:controller){ Class.new(ActionController::Base) }
+      let(:instance){ controller.new }
+
+      before { class Person < Resource; end }
+
+      context 'and the collection can be scoped' do
+        let(:collection){ mock(:scoped => [self]) }
+
+        before{ controller.expose(:person) }
+
+        it 'uses the existing collection method' do
+          instance.stubs(:people).returns(collection)
+          collection.expects(:new)
+          instance.person
+        end
+      end
+      context 'when the collection can not be scoped' do
+        let(:collection){ mock }
+
+        before{ controller.expose(:person) }
+
+        it 'falls back to the singularized constant' do
+          instance.stubs(:people).returns(collection)
+          Person.expects(:new)
+          instance.person
+        end
+      end
+    end
+
+    context 'when either :resource_id or :id are present in params' do
+      it "calls find with params[:resource_id] on the resource's class" do
+        Resource.expects(:find).with(42)
+        instance.resource
+      end
+
+      context 'when there is no :resource_id in params' do
+        before { instance.stubs(:params).returns({:id => 73}) }
+
+        it "calls find with params[:id] on the resource's class" do
+          Resource.expects(:find).with(73)
+          instance.resource
+        end
+      end
+    end
+    context 'when there are no ids in params' do
+      before do
+        instance.stubs(:params).returns({:resource => {:name => 'bob'}})
+      end
+
+      it 'calls new with params[:resouce_name]' do
+        Resource.expects(:new).with({:name => 'bob'})
+        instance.resource
+      end
     end
   end
 end
