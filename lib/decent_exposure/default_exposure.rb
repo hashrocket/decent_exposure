1a094481638f1fa4423a93da96c2b9e31b8119e9
Si Wilkins <si@yoomee.com>
2011-11-14 17:27:32 +0000

    Fixed problem with running rake gems:install (problem with Rails 2.3.5)

--- lib/decent_exposure/default_exposure.rb
+++ lib/decent_exposure/default_exposure.rb
@@ -7,20 +7,22 @@ module DecentExposure
       else
         klass.superclass_delegating_accessor(:_default_exposure)
       end
-      klass.default_exposure do |name|
-        collection = name.to_s.pluralize
-        if respond_to?(collection) && collection != name.to_s && send(collection).respond_to?(:scoped)
-          proxy = send(collection)
-        else
-          proxy = name.to_s.classify.constantize
-        end
-
-        if id = params["#{name}_id"] || params[:id]
-          proxy.find(id).tap do |r|
-            r.attributes = params[name] unless request.get?
+      if klass.respond_to?(:default_exposure)
+        klass.default_exposure do |name|
+          collection = name.to_s.pluralize
+          if respond_to?(collection) && collection != name.to_s && send(collection).respond_to?(:scoped)
+            proxy = send(collection)
+          else
+            proxy = name.to_s.classify.constantize
           end
-        else
-          proxy.new(params[name])
+
+          if id = params["#{name}_id"] || params[:id]
+            proxy.find(id).tap do |r|
+              r.attributes = params[name] unless request.get?
+            end
+          else
+            proxy.new(params[name])
+          end
         end
       end
     end
