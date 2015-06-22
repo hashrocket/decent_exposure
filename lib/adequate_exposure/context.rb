b57210d5efe8c04ed057a8da850a001a656909d2
Joshua Plicque <plicjo@gmail.com>
2015-06-22 16:05:33 -0400

    Document Context class

--- lib/adequate_exposure/context.rb
+++ lib/adequate_exposure/context.rb
@@ -2,14 +2,33 @@ module AdequateExposure
   class Context
     attr_reader :context, :attribute
 
+    # Public: Initialize a context.
+    #
+    # context   - The Class where the attribute is defined.
+    # attribute - The attribute that will be accessed by a getter
+    #             and setter.
     def initialize(context, attribute)
       @context, @attribute = context, attribute
     end
 
+    # Public: Read an attribute on the context Class.
+    #
+    # Get an attribute's value. If the attribute's instance
+    # variable is not defined, it will create one,
+    # execute attribute#fetch, and assign the result
+    # to the instance variable.
+    #
+    # Returns the attribute's value.
     def get
       ivar_defined?? ivar_get : set(fetch_value)
     end
 
+    # Public: Write to an attribute on the context Class.
+    #
+    # value - The value that will be set to the attribute's
+    #         instance variable.
+    #
+    # Returns the attribute's value.
     def set(value)
       ivar_set(value)
     end
