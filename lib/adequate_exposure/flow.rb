c9c9710f400fd930b6ea8c2fb7d13e4a1ac79e47
Joshua Plicque <plicjo@gmail.com>
2015-06-22 16:16:22 -0400

    Document Flow class

--- lib/adequate_exposure/flow.rb
+++ lib/adequate_exposure/flow.rb
@@ -2,12 +2,24 @@ module AdequateExposure
   class Flow
     attr_reader :controller, :options, :name
 
+    # Public: Initialize a Flow. This object responds to missing
+    # methods errors and attempts to delegate them to other objects.
+    #
+    # controller - The Controller class where the method was called.
+    # options    - The options Hash of the Exposure instance being called.
+    # name       - The String name of the Exposure instance.
     def initialize(controller, options)
       @controller = controller
       @options = options
       @name = options.fetch(:name)
     end
 
+    # Public: Attempts to re-delegate a method missing to the
+    # supplied block or the Behavior object.
+    #
+    # name  - The String name of the Exposure instance.
+    # *args - The arguments given for the missing method.
+    # block - The Proc invoked by the method.
     def method_missing(name, *args, &block)
       if respond_to_missing?(name)
         handle_flow_method(name, *args, &block)
@@ -16,6 +28,11 @@ module AdequateExposure
       end
     end
 
+    # Public: Checks if the Behavior class can handle the missing method.
+    #
+    # method_name     - The name of method that has been called.
+    # include_private - Prevents this method from catching calls to private
+    # method (default: false).
     def respond_to_missing?(method_name, include_private = false)
       Behavior.method_defined?(method_name) || super
     end
