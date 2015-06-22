0d3f2e18f0bc1e39316e1fcc081482ef1bb00b3c
Joshua Plicque <plicjo@gmail.com>
2015-06-22 16:10:24 -0400

    Document Controller class

--- lib/adequate_exposure/controller.rb
+++ lib/adequate_exposure/controller.rb
@@ -8,15 +8,42 @@ module AdequateExposure
     end
 
     module ClassMethods
+      # Public: Exposes an attribute to a controller Class.
+      #
+      # *args - An Array of attributes for the new exposure. See
+      #         Exposure#initialize for attribute details.
+      # block - If supplied, the exposed attribute method executes
+      #         the Proc when accessed.
+      #
+      # Returns the helper methods that are now defined on the class
+      # where this method is included.
       def expose(*args, &block)
         Exposure.expose! self, *args, &block
       end
 
+      # Public: Exposes an attribute to a controller Class.
+      # The exposed methods are then set to a before_action
+      # callback.
+      #
+      # name  - The String name of the Exposure instance.
+      # *args - An Array of attributes for the new exposure. See
+      #         Exposure#initialize for attribute details.
+      # block - If supplied, the exposed attribute method executes
+      #         the Proc when accessed.
+      #
+      # Sets the exposed attribute to a before_action callback in the
+      # controller.
       def expose!(name, *args, &block)
         expose name, *args, &block
         before_action name
       end
 
+      # Public: Configures an Exposure instance for a controller Class.
+      #
+      # name    - The String name of the Exposure instance.
+      # options - The Hash of options to configure the Exposure instance.
+      #
+      # Returns the exposure configuration Hash.
       def exposure_config(name, options)
         store = self.exposure_configuration ||= {}
         self.exposure_configuration = store.merge(name => options)
