f8dcacb8d1a4c41e4511f01cd2da8c965b7399bc
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-05-09 22:22:14 -0400

    Generate View scaffold templates for ERB engine

--- spec/generators/decent_exposure/scaffold_templates_generator_spec.rb
+++ spec/generators/decent_exposure/scaffold_templates_generator_spec.rb
@@ -4,9 +4,16 @@ RSpec.describe DecentExposure::Generators::ScaffoldTemplatesGenerator, type: :ge
 
   subject(:generator) { described_class.new }
 
-  it 'generates controller' do
-    allow(generator).to receive(:copy_file).with('controller.rb', 'lib/templates/rails/scaffold_controller/controller.rb')
+  context 'with erb' do
+    it 'generates controller and erb views' do
+      allow(generator).to receive(:copy_file).with('controller.rb', 'lib/templates/rails/scaffold_controller/controller.rb')
+      allow(generator).to receive(:copy_file).with('_form.html.erb', 'lib/templates/erb/scaffold/_form.html.erb')
+      allow(generator).to receive(:copy_file).with('edit.html.erb', 'lib/templates/erb/scaffold/edit.html.erb')
+      allow(generator).to receive(:copy_file).with('index.html.erb', 'lib/templates/erb/scaffold/index.html.erb')
+      allow(generator).to receive(:copy_file).with('new.html.erb', 'lib/templates/erb/scaffold/new.html.erb')
+      allow(generator).to receive(:copy_file).with('show.html.erb', 'lib/templates/erb/scaffold/show.html.erb')
 
-    generator.generate
+      generator.generate
+    end
   end
 end
